a2654ece94a554bde7725e7dd8477ddce5eee5fb
Adamides <andreas.adamides@capgemini.com>
2016-11-09 11:02:58 +0000

    added logging and props

--- src/main/java/com/andy/id/twittercrawler/KafkaProducer.java
+++ src/main/java/com/andy/id/twittercrawler/KafkaProducer.java
@@ -1,9 +1,15 @@
 package com.andy.id.twittercrawler;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
@@ -24,50 +30,39 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class KafkaProducer {
 	
+	private final static Logger LOG = LoggerFactory.getLogger(KafkaProducer.class);
+	private final static ConfigurationBuilder cb = new ConfigurationBuilder();
+	private final static String twitter_cfg_path = System.getProperty("user.home") + File.separator + "twitter_credentials.cfg";
+	private final static Properties twitter_props = new Properties();
+	private static InputStream input = null;
+	
 /*
  * to test run a local consumer:
  * ./kafka-console-consumer.sh ec2-52-212-186-139.eu-west-1.compute.amazonaws.com:9092 --topic footy_tweets --from-beginning --zookeeper ec2-52-212-186-139.eu-west-1.compute.amazonaws.com:2181
  */
 	
-	 public static void main(String[] args) throws TwitterException {
+	 public static void main(String[] args) {
+		
+		try{
+			input = new FileInputStream(twitter_cfg_path);
+			// load a properties file
+			twitter_props.load(input);
+			cb.setDebugEnabled(true);
+			cb.setOAuthConsumerKey(twitter_props.getProperty("ConsumerKey"));
+			cb.setOAuthConsumerSecret(twitter_props.getProperty("ConsumerSecret"));
+			cb.setOAuthAccessToken(twitter_props.getProperty("AccessToken"));
+			cb.setOAuthAccessTokenSecret(twitter_props.getProperty("AccessTokenSecret"));
+		}
+		catch(Exception e){
 			
-		final String teams[] = new String[20];
-		teams[0] = "arsenal";
-		teams[1] = "astonvilla";
-		teams[2] = "cardiff";
-		teams[3] = "chelsea";
-		teams[4] = "crystalpalace";
-		teams[5] = "everton";
-		teams[6] = "fulham";
-		teams[7] = "hull";
-		teams[8] = "liverpool";
-		teams[9] = "mcity";
-		teams[10] = "munited";
-		teams[11] = "newcastle";
-		teams[12] = "norwich";
-		teams[13] = "southampton";
-		teams[14] = "stoke";
-		teams[15] = "sunderland";
-		teams[16] = "swansea";
-		teams[17] = "tottenham";
-		teams[18] = "westbrom";
-		teams[19] = "westham";
-		
-		//final MySqlConn conn = new MySqlConn(db, user, psw, hostname, port);
+		}
 		
 		 
-		 ConfigurationBuilder cb = new ConfigurationBuilder();
-		 cb.setDebugEnabled(true);
-		 cb.setOAuthConsumerKey("");
-		 cb.setOAuthConsumerSecret("");
-		 cb.setOAuthAccessToken("");
-		 cb.setOAuthAccessTokenSecret("");
-		 
-		 TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+		TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
 		 
 		 /** Producer properties **/
 		Properties props = new Properties();
-		props.put("metadata.broker.list", "localhost:9092,localhost:9092");
+		props.put("metadata.broker.list", "localhost:9092");
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
 		props.put("request.required.acks", "1");
 			
@@ -75,28 +70,7 @@ public class KafkaProducer {
 			
 		final Producer<String, String> producer = new Producer<String, String>(config);
 		 
-		 String arsenal[] = {"#arsenal","#arsenalfc", "#afc", "#gunners", "#thegunners","#gooners","#comeongunners", "#comeongooners" ,"#comeonyougunners", "#coyg", "#comeonarsenal","#arsenalfamily", "#afcfamily"};
-		 String astonvilla[] = { "#astonvillafc", "#villans", "#avfc", "#astonvilla", "#govilla", "#comeonastonvilla", "#comeonvilla", "#comeonyouvillans", "#villafamily", "#avfcfamily"};
-		 String cardiff[] = { "#cardiff","#cardiffcity" , "#ccfc" , "#comeoncardiff" , "#cardifffc","#cardiffcityfamily","#cccfcfamily","#comeoncardiff","#comeonyoucardiff"};
-		 String chelsea[] = {"#chelsea","#chelseafc","#cfc","#ktbffh", "#gochelsea", "#cfcfamily", "#letsgochelsea", "#comeonchelsea" , "#comeoncfc", "#cfcfamily", "#chelseafcfamily","#cfcfamily"};
-		 String crystalpalace[] = {"#crystalpalace","#crystalpalacefc" , "#cpfc","#crystalpalacefamily","#cpfcfamily","#comeoncrystalpalace"};
-		 String everton[] = {"#everton", "#evertonfc", "#efc", "#evertonfamily", "#schoolofscience", "#toffees", "#toffeemen","#coyb", "#comeoneverton","#comeontoffees", "#goeverton","#efcfamily"};
-		 String fulham[] = { "#fulham","#fulhamfc", "#cottagers", "#ffc", "#gofulham","#fulhamfamily", "#comeonfulham", "#comeonyouwhites", "#coyws","#ffcfamily"};
-		 String hull[] = {"#hull","#hullfc", "#hullcityfc","#hcafc", "#hullcity", "#hcfc", "#comeonhull", "#hullcityfamily","#comeonyouhull"};
-		 String liverpool[] = {"#liverpool", "#liverpoolfc","#lfc","#goliverpool","#youwillneverwalkalone","#ynwa","#neverwalkalone","#comeonlfc", "#comeonliverpool", "#redordead", "#lfcfamily"};
-		 String mcity[] = {"#manchestercity", "#manchestercity", "#manchestercityfc","#mancity", "#mancityfc" ,"#citizens" ,"#mcfc","#gomcfc" ,"#gomancity" ,"#comeonmancity","#comeoncitizens","#mancityfamily","#mcfcfamily"};
-		 String munited[] = {"#manchesterunited", "#manchesterunitedfc", "#manutd", "#manunited", "#manunitedfc", "#reddevils" ,"#mufc", "#gomanutd", "#gomanunited", "#foreverunited" ,"#gomufc", "#gloryglorymanunited", "#ggmu","#manutdfamily","#mcfcfamily"};
-		 String newcastle[] = {"#newcastle","#newcastleunited","#nufc", "newcastlefc", "#nufcroundup","#comeonnewcastle","#toonarmy","#nufcfamily"};
-		 String norwich[] = {"#norwichfc", "#norwichcity", "#norwichcityfc", "#comeonyoucanaries", "#ncfc", "#comeonnorwich","#ncfcfamily"};
-		 String southampton[] = {"#southampton","#southamptonfc", "#saints", "#saintsfc", "#upthesaints", "#comeonsouthampton","southamptonfcfamily"};
-		 String stoke[] = {"#stoke","#stokecity", "#stokecityfc", "#scfc", "#comeonstoke", "#comeonyoustoke", "#comeonpotters", "#comeonyoupotters","#scfcfamily"};
-		 String sunderland[] = {"#sunderland", "#sunderlandfc", "#safc", "#mackems", "#comeonblackcats", "#comeonsunderland", "#comeonyoumackems", "#comeonyousunderland", "#comeonyoublackcats", "#gosunderland", "#goblackcats","#safcfamily"};
-		 String swansea[] = {"#swansea","#swanseacity","#swanseacityfc","#swanseafc", "#swans", "#swansfc", "#jackarmy", "#comeonswansea", "#twitterjacks","#comeonyoujacks"};
-		 String tottenham[] = {"#tottenham", "#tottenhamfc", "#tottenhamhotspur", "#tottenhamhotspurfc", "#yids", "#yidarmy", "#comeonyouspurs", "#coys", "#thfc", "#hotspurs", "#spursfamily","#thfcfamily"};
-		 String westbrom[] = {"#westbromwich","#westbromwichalbionfc", "#westbromwichfc", "#westbromfc", "#westbrom", "#baggies", "#wba", "#wbafc", "#wbfc", "#comeonbaggies", "#comeonyoubaggies", "#gowba", "#gobaggies"};
-		 String westham[] = {"#westham","#westhamunited", "#westhamunitedfc", "#whu", "#whufc","#westhamfamily", "#whtid", "#comeonwestham", "#comeonyouirons", "#coyi", "#comeonwestham", "#gowestham","#whufcfamily"};
-		 
-		 String all[] = {"#arsenal","#arsenalfc", "#afc", "#gunners", "#thegunners","#gooners","#comeongunners", "#comeongooners" ,"#comeonyougunners", "#coyg", "#comeonarsenal","#arsenalfamily", "#afcfamily",
+		final String all_hashtags[] = {"#arsenal","#arsenalfc", "#afc", "#gunners", "#thegunners","#gooners","#comeongunners", "#comeongooners" ,"#comeonyougunners", "#coyg", "#comeonarsenal","#arsenalfamily", "#afcfamily",
 				 "#astonvillafc", "#villans", "#avfc", "#astonvilla", "#govilla", "#comeonastonvilla", "#comeonvilla", "#comeonyouvillans", "#villafamily", "#avfcfamily",
 				 "#cardiff","#cardiffcity" , "#ccfc" , "#comeoncardiff" , "#cardifffc","#cardiffcityfamily","#cccfcfamily","#comeoncardiff","#comeonyoucardiff",
 				 "#chelsea","#chelseafc","#cfc","#ktbffh", "#gochelsea", "#cfcfamily", "#letsgochelsea", "#comeonchelsea" , "#comeoncfc", "#cfcfamily", "#chelseafcfamily","#cfcfamily",
@@ -117,47 +91,21 @@ public class KafkaProducer {
 				 "#westbromwich","#westbromwichalbionfc", "#westbromwichfc", "#westbromfc", "#westbrom", "#baggies", "#wba", "#wbafc", "#wbfc", "#comeonbaggies", "#comeonyoubaggies", "#gowba", "#gobaggies",
 				 "#westham","#westhamunited", "#westhamunitedfc", "#whu", "#whufc","#westhamfamily", "#whtid", "#comeonwestham", "#comeonyouirons", "#coyi", "#comeonwestham", "#gowestham","#whufcfamily"};
 		 
-		 final List<String[]> hashtags = new ArrayList<String[]>();
-		 hashtags.add(arsenal);
-		 hashtags.add(astonvilla); hashtags.add(cardiff);hashtags.add(chelsea);
-		 hashtags.add(crystalpalace);hashtags.add(everton); hashtags.add(fulham);hashtags.add(hull);
-		 hashtags.add(liverpool);hashtags.add(mcity); hashtags.add(munited);hashtags.add(newcastle);
-		 hashtags.add(norwich);hashtags.add(southampton); hashtags.add(stoke);hashtags.add(sunderland);
-		 hashtags.add(swansea);hashtags.add(tottenham); hashtags.add(westbrom);hashtags.add(westham);
-		 
+			 
 		 StatusListener listener = new StatusListener() {
 			
-			int totalTweets = 0;
-			String createdAt,userScreenName,tweet,coord;
-			int userFollowers,retweetCount,favoriteCount;
-			
+			String createdAt,userScreenName,tweet,coord,kafka_message;
+			int userFollowers,retweetCount,favoriteCount;			
 			String regex = "\\'";
-			String replacement = "";
+			String replacement = "";			
 			
-			String kafka_message;
-			
-			public void onException(Exception arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			public void onDeletionNotice(StatusDeletionNotice arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
 			public void onStatus(Status status) {
 						
 				if (status.getLang().equals("en")){
-			     	totalTweets++;
-			    	
 					createdAt  = status.getCreatedAt().toString();						
 					userScreenName = status.getUser().getScreenName().toString();	
 					tweet = status.getText().toString();
-					if(String.valueOf(totalTweets).matches("([1][0]*|[5][0]*)")){
-			    		System.out.println(createdAt + "  :  "+totalTweets +" tweets retieved so far!");
-			    		System.out.println("The " +totalTweets+"th retrieved tweet is: " + tweet);
-			    	};
+					
 					if(status.getGeoLocation()!=null){
 						double lat = status.getGeoLocation().getLatitude();
 						double lon = status.getGeoLocation().getLongitude();
@@ -171,8 +119,7 @@ public class KafkaProducer {
 					favoriteCount = status.getFavoriteCount();
 					
 					//System.out.println("Tweet:" + tweet);
-					//need to remove apostrophes
-					
+										
 					tweet = tweet.replaceAll(regex,replacement);
 					
 					kafka_message="\""+tweet+"\","
@@ -182,8 +129,7 @@ public class KafkaProducer {
 							+userFollowers+","
 							+retweetCount+","
 							+favoriteCount+","
-							;
-					
+							;				
 					
 					KeyedMessage<String, String> data = 
 							new KeyedMessage<String, String>("footy_tweets", kafka_message);//DataObjectFactory.getRawJSON(status));
@@ -191,25 +137,19 @@ public class KafkaProducer {
 					
 					//check if tweet is here
 					//*****need to think about retweets count
-					/*int i = 0;
-					for(String[] array : hashtags){							
-						for(String hashtag:array){							
-							if(StringUtils.containsIgnoreCase(tweet, hashtag)){
-							//if (tweet.contains(hashtag)){
-							
-							KeyedMessage<String, String> data = 
-									new KeyedMessage<String, String>("kafka.topic", kafka_message);//DataObjectFactory.getRawJSON(status));
-							producer.send(data);
-							//(previously was adding new tweets to SQL table)
-							}
-							
-						}
-						i++;
-					}*/
 
-				}				
+				}
+				
+			}
+			
+			public void onException(Exception arg0) {
+				// TODO Auto-generated method stub				
 			}
 
+			public void onDeletionNotice(StatusDeletionNotice arg0) {
+				// TODO Auto-generated method stub				
+			}
+			
 			public void onTrackLimitationNotice(int arg0) {
 				// TODO Auto-generated method stub
 				
@@ -228,13 +168,11 @@ public class KafkaProducer {
 		 };
 		 
 		 FilterQuery fq = new FilterQuery();
-		 
-		 fq.track(all);
-		 
+		 fq.track(all_hashtags);
 		 twitterStream.addListener(listener);
-		 System.out.println("Listener added!");
+		 LOG.info("Listener Added!!");
 		 twitterStream.filter(fq); 
-		 System.out.println("Filters set!");		 
+	     LOG.info("Filters set!");
 		 
 	 }
 }
